ffd7fbae3a87f236c12d9991f3bd942f0070b4f9
Nishino0719 <nishino0719@icloud.com>
2020-12-31 18:34:03 +0900

    :zap: feat: add tailwind config variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // 'media' or 'class'
   theme: { extend: { colors: { 'accent-1': '#333' } } },
-  variants: { extend: {} },
+  variants: [
+    'responsive',
+    'group-hover',
+    'focus-within',
+    'first',
+    'last',
+    'odd',
+    'even',
+    'hover',
+    'focus',
+    'active',
+    'visited',
+    'disabled'
+  ],
   plugins: []
 }
