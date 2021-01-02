ca796827eb81e1837fa1bb852571e0468b3c34ca
Nishino0719 <nishino0719@icloud.com>
2021-01-02 21:00:47 +0900

    :hammer: feat: add 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { Layout } from '../components/layout'
+
+const Error = () => {
+  return (
+    <Layout>
+      <main className="profile-page">
+        <section className="text-gray-600 body-font">
+          <div className="">
+            <h1>エラーです。</h1>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Error
