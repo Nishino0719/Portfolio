487cc415213da1924bd77e3e418ad44c10d8e742
Nishino0719 <nishino0719@icloud.com>
2021-01-02 11:13:53 +0900

    :hammer: feat : add works template

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -5,79 +5,78 @@ const Works = () => {
   return (
     <Layout>
       <main className="profile-page">
-        <section className="relative block" style={{ height: '500px' }}>
-          <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')"
-            }}
-          >
-            <span
-              id="blackOverlay"
-              className="w-full h-full absolute opacity-50 bg-black"
-            ></span>
-          </div>
-        </section>
-        <section className="relative py-16 bg-gray-300">
-          <div className="container mx-auto px-4">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
-              <div className="px-6">
-                <div className="flex flex-wrap justify-center">
-                  <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                    <div className="relative">
-                      <img
-                        alt="..."
-                        src="https://avatars0.githubusercontent.com/u/55534054?s=460&u=402783902455ae84995129488dd3a12d0699fd84&v=4"
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
-                        style={{ maxWidth: '150px' }}
-                      />
-                    </div>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap -mx-4 -my-8">
+              <div className="py-8 px-4 lg:w-1/3">
+                <div className="h-full flex items-start">
+                  <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                    <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">
+                      Jul
+                    </span>
+                    <span className="font-medium text-lg text-gray-800 title-font leading-none">
+                      18
+                    </span>
                   </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center lg:block hidden">
-                    <div className="py-6 px-3 mt-32 sm:mt-0">
-                      <button
-                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
-                        type="button"
-                        style={{ transition: 'all .15s ease' }}
-                      >
-                        お問い合わせ
-                      </button>
-                    </div>
-                  </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-1 ">
-                    <div className="justify-center py-4 lg:pt-4 pt-8"></div>
+                  <div className="flex-grow pl-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-pink-500 mb-1">
+                      CATEGORY
+                    </h2>
+                    <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
+                      The 400 Blows
+                    </h1>
+                    <p className="leading-relaxed mb-5">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings
+                      jianbing microdosing tousled waistcoat.
+                    </p>
                   </div>
                 </div>
-                <div className="text-center mt-10">
-                  <h3 className="text-3xl font-semibold leading-normal text-gray-800 mb-2">
-                    Nishino
-                  </h3>
-                  <div className="text-xs leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-                    Japan Fukuoka
+              </div>
+              <div className="py-8 px-4 lg:w-1/3">
+                <div className="h-full flex items-start">
+                  <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                    <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">
+                      Jul
+                    </span>
+                    <span className="font-medium text-lg text-gray-800 title-font leading-none">
+                      18
+                    </span>
                   </div>
-                  <div className="mb-2 text-gray-700 mt-10 font-semibold text-sm">
-                    Front Engineer | Mobile Engineer | Blockchain Engineer
+                  <div className="flex-grow pl-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-pink-500 mb-1">
+                      CATEGORY
+                    </h2>
+                    <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
+                      Shooting Stars
+                    </h1>
+                    <p className="leading-relaxed mb-5">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings
+                      jianbing microdosing tousled waistcoat.
+                    </p>
                   </div>
                 </div>
-                <div className="mt-10 py-10 border-t border-gray-300 text-center">
-                  <div className="flex flex-wrap justify-center">
-                    <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-gray-800">
-                        An artist of considerable range, Jenna the name taken by
-                        Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                        performs and records all of his own music, giving it a
-                        warm, intimate feel with a solid groove structure. An
-                        artist of considerable range.
-                      </p>
-                      <a
-                        href="#pablo"
-                        className="font-normal text-pink-500"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Show more
-                      </a>
-                    </div>
+              </div>
+              <div className="py-8 px-4 lg:w-1/3">
+                <div className="h-full flex items-start">
+                  <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                    <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">
+                      Jul
+                    </span>
+                    <span className="font-medium text-lg text-gray-800 title-font leading-none">
+                      18
+                    </span>
+                  </div>
+                  <div className="flex-grow pl-6">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-pink-500 mb-1">
+                      CATEGORY
+                    </h2>
+                    <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
+                      Neptune
+                    </h1>
+                    <p className="leading-relaxed mb-5">
+                      Photo booth fam kinfolk cold-pressed sriracha leggings
+                      jianbing microdosing tousled waistcoat.
+                    </p>
                   </div>
                 </div>
               </div>
